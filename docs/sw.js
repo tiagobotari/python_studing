c0e8f9a34da19f6098c9b7d01accfe77b9620afd
tiagobotari <tiagobotari@gmail.com>
2026-02-03 01:02:45 +0100

    Improving the webpage for the flashcards, removing distractions.

---
+++ docs/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'flashcards-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/python.html',
+  '/deep-learning.html',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
